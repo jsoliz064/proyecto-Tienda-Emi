4f489d19275425791b80191a5eb975dc0f790c88
jsoliz064 <jsoliz064@gmail.com>
2021-07-06 17:51:00 -0400

    asdfasdd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,13 @@ class DatabaseSeeder extends Seeder
         $user5->name = 'harold';
         $user5->email= 'harold@gmail.com';
         $user5->password = bcrypt('12345678');
-        $user5->assignRole('Admin');
+        //$user5->assignRole('Admin');
+        $user5->save();
 
+        $user6 = new user();
+        $user6->name = 'daniel';
+        $user6->email= 'jsoliz064@gmail.com';
+        $user6->password = bcrypt('12345678');
+        $user6->save();
     }
 }
