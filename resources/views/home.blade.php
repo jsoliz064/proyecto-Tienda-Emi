8a7ce23869c5c9b56bab49c4f42569e0ed1fbb63
MariaInesB7 <mariainesbb2000@gmail.com>
2021-07-06 16:39:25 -0400

    Vista Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,51 @@
 
 @section('content')
     <p>BIENVENIDO AL PANEL DE ADMINISTRADOR.</p>
+    <head>
+        <link rel="stylesheet" href="public/css/home.css">
+    </head>
+    <body>
+    <div class= "container">
+        <div class="prueba">
+        <h2 class "title"> Tienda Emi </h2>
+        </div>
+        <div class= "menu" >
+        
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg"  >
+        </div>
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg">
+        
+        </div>
+        
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg">
+        </div>
+        
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg">
+        </div>
+        
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg">
+        </div>
+        
+        <div class= "image">
+        <img src= "vendor/adminlte/dist/img/img1.jpeg">
+        </div>
+        
+        </div>
+    </body>
+
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+   {{--  <link rel="stylesheet" href="/css/home.css"> --}}
+  
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+   
 @stop
 
