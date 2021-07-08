94fad94d5f5ca4af5fdc734486809e8f2970c9ef
MariaInesB7 <mariainesbb2000@gmail.com>
2021-07-08 00:05:15 -0400

    migracion Personal

--- database/migrations/2021_07_08_035404_create_personals_table.php
+++ database/migrations/2021_07_08_035404_create_personals_table.php
@@ -15,6 +15,12 @@ class CreatePersonalsTable extends Migration
     {
         Schema::create('personals', function (Blueprint $table) {
             $table->id();
+            $table->string('ci');
+            $table->string('nombre');
+            $table->char('sexo');
+            $table->string('telefono')
+            $table->string('email')
+            $table->string('domicilio')
             $table->timestamps();
         });
     }
