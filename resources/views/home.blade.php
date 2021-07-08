1d37c0e8f51ebdb5de5c9e2c370d930ce19b1cf4
MariaInesB7 <mariainesbb2000@gmail.com>
2021-07-08 17:58:32 -0400

    Accesos rapidos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,47 +7,52 @@
 @stop
 
 @section('content')
-
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,900&display=swap" rel="stylesheet">
+</head>
 <body>
-	
+	<!--cliente, proveedor, productos¿,personal-->
 	<div class= "container">
+		<div class="cabecera"style="display: flex">
+	<i class="fas fa-car-side fa-5x" style="color:#111111" > </i>
+		<h1 class= "title"> TIENDA EMI </h1>
+	</div>
 	<div class="prueba">
-	<h2 class= "title"> Tienda Emi </h2>
+	<h2> Bienvenido</h2>
 	</div>
 	<div class= "menu" >
 	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
-    <a href="{{route('clientes.index')}}">
-    </a>
-	</div>
-	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
+	<div class= "iconos">
 	<a href="{{route('clientes.index')}}">
+	<i class="fas fa-user-alt fa-7x" style="color:#364542" ></i> 
+    </a>
+	<h4> Clientes</h4>
 	</div>
 	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
-    <a href="{{route('proveedores.index')}}">
+	<div class= "iconos">
+		<a href="{{route('proveedores.index')}}">
+			<i class="fas fa-dolly fa-7x" style="color:#364542" ></i> 
+			</a>
+	<h4> Proveedores</h4>
 	</div>
 	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
-    <a href="{{route('proveedores.index')}}">
+	<div class= "iconos">
+		<a href="{{route('clientes.index')}}">
+			<i class="fas fa-box-open fa-7x" style="color:#364542"></i>
+			</a>
+	<h4> Productos</h4>
 	</div>
 	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
-    <a href="{{route('proveedores.index')}}">
+	<div class= "iconos">
+		<a href="{{route('personales.index')}}">
+			<i class="fas fa-user-tie fa-7x" style="color:#364542" ></i> 
+			</a>
+	<h4> Personal</h4>
 	</div>
 	
-	<div class= "image">
-	<img src= "vendor/adminlte/dist/img/img1.jpeg">
-    <a href="{{route('proveedores.index')}}">
-	</div>
 	
-	</div>
 	
 </body>
 
