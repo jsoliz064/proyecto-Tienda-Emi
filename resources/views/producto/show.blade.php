99bfe97705d20d436413519d1b3f68f0b72cf0d0
MarkCruz911 <85602094+MarkCruz911@users.noreply.github.com>
2021-07-26 00:38:20 -0400

    mark

--- resources/views/producto/show.blade.php
+++ resources/views/producto/show.blade.php
@@ -6,6 +6,51 @@
 @stop
 
 @section('content')
+
+<section id="container">
+    <div class="title_page">
+        <h1><i class="fas fa-cube"></i> Nueva Venta</h1>
+    </div>
+    <div class="datos_cliente">
+        <div class="action_cliente">
+            <h4>Datos del Cliente</h4>
+            <h6>{{$producto->codigo}}</h6>
+            {{--<a href="#" class="btn_new btn_new_cliente"><i class="fas fa-plus"></i>Nuevo Cliente</a>--}}
+        </div>
+        <form name="form_new_cliente_venta" id="form_new_cliente_venta" class="datos">
+            <input type="hidden" name="action" value="addCliente">
+            <input type="hidden" name="idcliente" value="idcliente" value="" required>
+            <div class="wd30">
+                <label>NIT</label>
+                <input type="text" name="nit_cliente" id="nit_cliente" disabled required>
+                <h4>{{$producto->codigo}}</h4>
+            </div>
+            <div class="wd30">
+                <label>Nombre</label>
+                {{--<input type="text" name="nit_cliente" id="nit_cliente">--}}
+                <h4>{{$producto->codigo}}</h4>
+            </div>
+            <div class="wd30">
+                <label>Telefono</label>
+                {{--<input type="text" name="nit_cliente" id="nit_cliente">--}}
+                <h4>{{$producto->codigo}}</h4>
+            </div>
+            <div class="wd100">
+                <label>Direccion</label>
+                {{--<input type="text" name="nit_cliente" id="nit_cliente">--}}
+                <h4>{{$producto->codigo}}</h4>
+            </div>
+            
+        </form>
+    </div>
+</section>
+
+
+
+
+
+
+
 <div class="container " style="background-color: white">
     <div class="row justify-content-center border rounded-top">
         <div class="col">
