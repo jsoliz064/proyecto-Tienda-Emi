895b6b710b53b0192fd4907c2ca2762e1027eb52
jsoliz064 <jsoliz064@gmail.com>
2021-08-16 00:25:13 -0400

    recibo

---
+++ app/Http/Controllers/ReciboPagoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReciboPago;
+use Illuminate\Http\Request;
+
+class ReciboPagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReciboPago  $reciboPago
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReciboPago $reciboPago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ReciboPago  $reciboPago
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReciboPago $reciboPago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReciboPago  $reciboPago
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReciboPago $reciboPago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReciboPago  $reciboPago
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReciboPago $reciboPago)
+    {
+        //
+    }
+}
--- resources/views/cuota/index.blade.php
+++ resources/views/cuota/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Inicio')
 
 @section('content_header')
-<h1>titulo</h1>
+<h1>Cuotas</h1>
 @stop
 
 @section('content')
